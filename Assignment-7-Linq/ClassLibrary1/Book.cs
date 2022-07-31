6faf8f80b5b5d22533f77a50da1a45235e883e80
morciu <marianpralea@gmail.com>
2022-07-31 16:47:31 +0300

    Assignment 7

---
+++ Assignment-7-Linq/ClassLibrary1/Book.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassLibrary1
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public bool Read { get; set; }
+        public override string ToString()
+        {
+            return Read ?
+                $"{Title} by {Author} has {Pages} pages and you have read it already." :
+                $"{Title} by {Author} has {Pages} pages and you haven't read yet.";
+        }
+        static public List<Book> GenerateBookDB()
+        {
+            return new List<Book>
+            {
+                new Book { Id = 1, Title = "Dracula", Author = "Bram Stoker", Pages = 418, Read = false },
+                new Book { Id = 2, Title = "Moby Dick", Author = "Herman Melville", Pages = 752, Read = true },
+                new Book { Id = 3, Title = "Frankenstein", Author = "Mary Shelley", Pages = 280, Read = true },
+                new Book { Id = 4, Title = "The Dog It Was That Died", Author = "Tom Stoppard", Pages = 192, Read = true },
+                new Book { Id = 5, Title = "Get in the van", Author = "Henry Rollins", Pages = 247, Read = false },
+                new Book { Id = 6, Title = "Heart of Darkness", Author = "Joseph Conrad", Pages = 72, Read = false },
+                new Book { Id = 7, Title = "Charlie and the Chocolate Factory", Author = "Roald Dahl", Pages = 176, Read = true },
+                new Book { Id = 8, Title = "Great Expectations", Author = "Charles Dickens", Pages = 544, Read = true },
+                new Book { Id = 9, Title = "The Adventures of Tom Sawyer", Author = "Mark Twain", Pages = 216, Read = true },
+                new Book { Id = 10, Title = "The Turn of the Screw", Author = "Henry James", Pages = 174, Read = true },
+                new Book { Id = 11, Title = "Strange Case of Dr Jekyll and Mr Hyde", Author = "R. L. Stevenson", Pages = 141, Read = false },
+                new Book { Id = 12, Title = "The Trial", Author = "Franz Kafka", Pages = 106, Read = true },
+                new Book { Id = 13, Title = "Bartleby, the Scrivener", Author = "Herman Melville", Pages = 68, Read = false }
+            };
+        }
+    }
+}
---
+++ Assignment-7-Linq/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using ClassLibrary1;
+
+var books = Book.GenerateBookDB();
+
+
+// Where, OrderBy, Select
+var filteredBooksByPages = books.
+    Where(book => book.Pages < 200 && book.Pages > 100).
+    OrderBy(book => book.Pages).
+    Select(book => book.Title);
+
+Console.WriteLine("\nBooks between 100 and 200 pages");
+foreach(var book in filteredBooksByPages)
+{
+    Console.WriteLine(book);
+}
+
+Console.WriteLine("\n");
+
+
+// Grouping
+var groupedBooksByRead = books.
+    GroupBy(book => book.Read);
+
+Console.WriteLine("\nGrouping books into read/unread");
+foreach (var group in groupedBooksByRead)
+{
+    if (group.Key == true) { Console.WriteLine($"Read books:"); }
+    else { Console.WriteLine($"Unread books:"); }
+
+    foreach (var book in group)
+    {
+        Console.WriteLine($"{book.Title}: {book.Author}");
+    }
+    Console.WriteLine("\n");
+}
+
+// Conversion
+var bookArray = books.ToArray();
+
+Console.WriteLine("\nConverting books to an array and displaying the books with even page numbers");  
+for (int i = 0; i < bookArray.Length; i++)
+{
+    if (i % 2 == 0) { Console.WriteLine(bookArray[i]); }
+}
+
+// Element Operatos
+Console.WriteLine("\nFirst Book under 100 pages.");
+var firstSmallBook = books.First(book => book.Pages < 100);
+Console.WriteLine(firstSmallBook);
+
+// Aggregation
+var nrOfBooks = books.Count();
+Console.WriteLine("\n Counting the nr of books");
+Console.WriteLine(nrOfBooks);
+
+// Quantifiers
+var authorsWithHInName = books.
+    Where(book => book.Author.ToLower().Contains("h")).
+    Select(book => book.Author);
+
+Console.WriteLine("\nAuthors with 'H' in their name");
+foreach(var author in authorsWithHInName)
+{
+    Console.WriteLine(author);
+}
+
